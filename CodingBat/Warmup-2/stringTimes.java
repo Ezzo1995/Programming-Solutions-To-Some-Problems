4d86993bff9481882557dd08b7e4534f1d6bf110
ahmed_sherif95 <ahmed_sherif95@hotmail.com>
2018-06-26 13:23:34 +0200

    Commit 1 Warmup-2

---
+++ CodingBat/Warmup-2/stringTimes.java
@@ -0,0 +1,20 @@
+// Codingbat warmup-1 problem : stringTimes 
+// Problem Link: http://codingbat.com/prob/p142270
+
+public String stringTimes(String str, int n)
+{
+  String result = "";
+  
+  for(int i=0; i<n; i++)
+  {
+    result += str;
+  }
+  
+  return result;
+}
+
+
+
+
+
+
